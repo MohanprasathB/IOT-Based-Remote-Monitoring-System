2ce3596b9d41954e55d5ae7a8e13ee44fba96da3
Mpcontroller <mohanprasathbme@gmail.com>
2025-02-01 10:27:17 +0530

    first commit

---
+++ CM7/Core/Inc/Master_Modbus.h
@@ -0,0 +1,68 @@
+#ifndef INC_MODBUSMASTER_H_
+#define INC_MODBUSMASTER_H_
+
+/* Modbus Master Configuration */
+#define MODBUS_MASTER_INBOX_LENGTH         256  // Maximum response size
+#define MODBUS_MASTER_OUTBOX_LENGTH        256  // Maximum request size
+#define MODBUS_MASTER_TIMEOUT_MS           100  // Response timeout in milliseconds
+#define MODBUS_MAX_REGISTERS               128  // Maximum registers to read/write
+
+/* Function Code */
+#define MODBUS_FC_READ_INPUT_REGISTERS     0x04  // Read Input Registers
+
+/* RS-485 Line Selection */
+#define RS485_TX_ENABLE()                  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET)
+#define RS485_RX_ENABLE()                  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET)
+
+/* Master-Specific Variables */
+extern unsigned char ModbusMaster_inbox[MODBUS_MASTER_INBOX_LENGTH];
+extern unsigned char ModbusMaster_outbox[MODBUS_MASTER_OUTBOX_LENGTH];
+extern unsigned short ModbusMaster_Tx_index;
+extern unsigned short ModbusMaster_Rx_index;
+extern unsigned short ModbusMaster_TimeoutFlag;
+extern unsigned short ModbusMaster_FrameComplete_Flag;
+//extern unsigned char ModbusMaster_outbox[];
+
+/* Structure to Handle Modbus Frames */
+typedef struct
+{
+    union
+    {
+        unsigned short Data;
+        struct
+        {
+            unsigned short LSB : 8;
+            unsigned short MSB : 8;
+        } bit;
+    } byte;
+} Modbus_Split;
+
+typedef struct
+{
+    union
+    {
+        unsigned short array[6];
+        struct
+        {
+            unsigned int SlaveID           : 8;  // Slave ID
+            unsigned int FunctionCode      : 8;  // Function Code
+            unsigned int StartAddress      : 16; // Start Address
+            unsigned int DataLength        : 16; // Data Length
+            unsigned int crc_value         : 16; // CRC
+        } bit;
+    } frame;
+} Modbus_MasterFrame;
+
+/* Extern Variables */
+extern Modbus_MasterFrame ModbusFrame;
+
+/* Function Prototypes */
+void ModbusMaster_Init(void);
+unsigned short ModbusMaster_CalculateCRC(unsigned char *buffer, unsigned short length);
+void ModbusMaster_SendRequest(unsigned char slave_id, unsigned char function_code, unsigned short start_address, unsigned short data_length, unsigned short *data);
+unsigned short ModbusMaster_ReceiveResponse(unsigned char *buffer, unsigned short length);
+
+/* Read Input Registers Function */
+void ModbusMaster_ReadInputRegisters(unsigned char slave_id, unsigned short start_address, unsigned short data_length);
+
+#endif /* INC_MODBUSMASTER_H_ */
---
+++ CM7/Core/Src/Master_Modbus.c
@@ -0,0 +1,125 @@
+#include "Master_Modbus.h"
+#include "stm32h7xx_hal.h"
+#include <stdio.h>
+#include "main.h"
+
+/* Define the index and flags */
+unsigned short ModbusMaster_Tx_index = 0;
+unsigned short ModbusMaster_Rx_index = 0;
+unsigned short ModbusMaster_TimeoutFlag = 0;
+unsigned short ModbusMaster_FrameComplete_Flag = 0;
+unsigned char ModbusMaster_outbox[MODBUS_MASTER_OUTBOX_LENGTH];
+/* Define the Modbus Frame structure */
+Modbus_MasterFrame ModbusFrame;
+extern UART_HandleTypeDef huart1;
+
+/* Initialize Modbus USART and GPIO */
+void ModbusMaster_Init(void)
+{
+    /* Initialize GPIO for RS-485 */
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    GPIO_InitStruct.Pin = GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    /* Initialize UART1 for Modbus communication */
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+    {
+        printf("UART1 initialization failed!\n");
+    }
+}
+
+/* Calculate CRC for Modbus frame */
+unsigned short ModbusMaster_CalculateCRC(unsigned char *buffer, unsigned short length)
+{
+    unsigned short crc = 0xFFFF;
+    for (unsigned short pos = 0; pos < length; pos++)
+    {
+        crc ^= buffer[pos];
+        for (int i = 0; i < 8; i++)
+        {
+            if (crc & 1)
+                crc = (crc >> 1) ^ 0xA001;
+            else
+                crc >>= 1;
+        }
+    }
+    return crc;
+}
+
+/* Send a Modbus request */
+void ModbusMaster_SendRequest(unsigned char slave_id, unsigned char function_code, unsigned short start_address, unsigned short data_length, unsigned short *data)
+{
+    ModbusMaster_Tx_index = 0;
+
+    /* Construct the Modbus frame */
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = slave_id;
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = function_code;
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = (start_address >> 8) & 0xFF;
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = start_address & 0xFF;
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = (data_length >> 8) & 0xFF;
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = data_length & 0xFF;
+
+    /* Calculate CRC */
+    unsigned short crc = ModbusMaster_CalculateCRC(ModbusMaster_outbox, ModbusMaster_Tx_index);
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = crc & 0xFF;
+    ModbusMaster_outbox[ModbusMaster_Tx_index++] = (crc >> 8) & 0xFF;
+
+    // Debug
+    printf("Modbus Request: ");
+    for (int i = 0; i < ModbusMaster_Tx_index; i++)
+    {
+        printf("%02X ", ModbusMaster_outbox[i]);
+    }
+    printf("\n");
+
+    /* Enable TX and send the frame */
+    RS485_TX_ENABLE();
+    HAL_UART_Transmit(&huart1, ModbusMaster_outbox, ModbusMaster_Tx_index, HAL_MAX_DELAY);
+    RS485_RX_ENABLE();
+}
+
+unsigned short ModbusMaster_ReceiveResponse(unsigned char *buffer, unsigned short length)
+{
+    if (HAL_UART_Receive(&huart1, buffer, length, MODBUS_MASTER_TIMEOUT_MS) == HAL_OK)
+    {
+        // Debug: Print the received buffer
+        printf("Received Buffer: ");
+        for (int i = 0; i < length; i++) {
+            printf("%02X ", buffer[i]);
+        }
+        printf("\n");
+
+        // Validate CRC
+        unsigned short crc_received = (buffer[length - 2] << 8) | buffer[length - 1];
+        unsigned short crc_calculated = ModbusMaster_CalculateCRC(buffer, length - 2);
+
+        // Debug: Print the CRC values
+        printf("CRC Received: 0x%04X, CRC Calculated: 0x%04X\n", crc_received, crc_calculated);
+
+        if (crc_received == crc_calculated)
+        {
+            ModbusMaster_FrameComplete_Flag = 1;
+            return length;
+        }
+    }
+
+    ModbusMaster_TimeoutFlag = 1;
+    return 0;
+}
+
+/* Read Input Registers */
+void ModbusMaster_ReadInputRegisters(unsigned char slave_id, unsigned short start_address, unsigned short data_length)
+{
+     ModbusMaster_SendRequest(slave_id, MODBUS_FC_READ_INPUT_REGISTERS, start_address, data_length, NULL);
+}
